a1a6ecff23d612027a7a3e1583d663a38a1da871
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2021-10-28 22:23:06 -0700

    Set up AppSettings configuration

--- BardicInspiration/Program.fs
+++ BardicInspiration/Program.fs
@@ -2,7 +2,18 @@ namespace Archipendulum.BardicInspiration
 
 module Program =
 
+    open System.IO
+    open Microsoft.Extensions.Configuration
+
+    let config =
+        ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("AppSettings.json", true, true)
+            .Build()
+
     [<EntryPoint>]
     let main argv =
         printfn "Starting"
+        let token = config.["Token"]
+        printfn "%s" token
         1
